6b7937156db08376a6fdb8f000f6bf8e63270627
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-06-03 13:08:31 -0300

    Add better ui to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ export default function App() {
     <div className="container">
       <h1>Create a post</h1>
       <PostCreate />
+      <hr />
+      <h2>Posts</h2>
       <PostList />
     </div>
   );
