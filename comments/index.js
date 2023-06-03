be1a1d5cad47bf4e1384bddc3ae38d62dd35b278
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-06-03 01:30:47 -0300

    fix cors bugs on both posts and comments microservices

--- comments/index.js
+++ comments/index.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 
 const { randomBytes } = require('crypto');
 
+const cors = require('cors');
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 //----------------------------------------------------------------------------
 //Our posts "database" for this toy project
 const posts = {};
