46d8afb51bbec19e1da32439ccdda09cc1d26805
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-06-03 01:29:22 -0300

    implement PostList into app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import PostCreate from '../src/components/PostCreate';
+import PostList from './components/PostList';
 
 export default function App() {
   return (
     <div className="container">
       <h1>Create a post</h1>
       <PostCreate />
+      <PostList />
     </div>
   );
 }
